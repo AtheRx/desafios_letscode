03fe330cda5fdd9a435cdd936dd5e1fd1a258aec
Victor Hugo <victor7k7k@gmail.com>
2022-07-03 16:24:05 -0300

    removendo imports desnecessarios

--- src/desafio2/Desafio2.java
+++ src/desafio2/Desafio2.java
@@ -1,14 +1,6 @@
 package desafio2;
 
-import java.text.CollationElementIterator;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 import java.util.TreeSet;
-import java.util.stream.Collectors;
 
 public class Desafio2 {
 

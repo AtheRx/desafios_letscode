083f0df5bcb6a513c5d72a13b2b6dbd928f7d061
Victor Hugo <victor7k7k@gmail.com>
2022-07-04 17:19:25 -0300

    Ajuste resolucao desafio 4

--- src/desafio4/Desafio4.java
+++ src/desafio4/Desafio4.java
@@ -20,26 +20,30 @@ public class Desafio4 {
             [1, 2, 3, 4, 5, 6, 7, 8, 12, 15]
          */
 
-
         int[] array1 = {1, 3, 5, 7};
         int[] array2 = {2, 4, 6, 8, 12, 15};
 
-        int[] array3 = new int[array1.length + array2.length];
+        juntarArrays(array1, array2);
+        
+    }
+
+    public static void juntarArrays(int[] array1, int[] array2) {
+        int[] arrayConsolidado = new int[array1.length + array2.length];
         String saidaFormatada;
 
         for (int i = 0; i < array1.length; i ++){
-            array3[i] = array1[i];
+            arrayConsolidado[i] = array1[i];
         }
 
         int cont = 0;
-        for (int i =  array1.length; i < array3.length; i ++ ){
-            array3[i] = array2[cont];
+        for (int i =  array1.length; i < arrayConsolidado.length; i ++ ){
+            arrayConsolidado[i] = array2[cont];
             cont ++;
         }
 
-        saidaFormatada = String.format("[%d", array3[0]);
-        for (int i : array3) {
-            if(i != array3[0]){
+        saidaFormatada = String.format("[%d", arrayConsolidado[0]);
+        for (int i : arrayConsolidado) {
+            if(i != arrayConsolidado[0]){
                 saidaFormatada += String.format(",%d", i);
             }
         }
